94c2feace99af73481426b22c52c1d6dbf426cf1
Artem-Pb <kenni506@icloud.com>
2025-06-23 08:53:40 +0300

    common2

--- RPN_Calculator/src/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "struct.h"
-
-int parse_expression(const char *src, TokenType *tokens);
-int is_function(const char *str);
-
-#endif
--- RPN_Calculator/src/type.h
+++
@@ -1,8 +0,0 @@
-#ifndef MY_CTYPE_H
-#define MY_CTYPE_H
-
-int isspace(int c);
-int isdigit(int c);
-int isalpha(int c);
-
-#endif
