28737454b6ce5fe014b7535c41b803a9673ca1d1
Artem-Pb <kenni506@icloud.com>
2025-06-24 10:12:51 +0300

    common

--- JavaRush/out/production/javarush-project/ru/javarush/system/linux/level02/task06/solution.sh
+++
@@ -1,8 +0,0 @@
-# Измените имя пользователя `new_user` на `developer`.
-usermod -l developer new_user
-
-# Смените домашнюю директорию пользователя на `/home/developer` с переносом всех данных.
-usermod -d /home/developer -m developer
-
-# Визуально проверьте изменения в файле `/etc/passwd`.
-grep developer /etc/passwd
